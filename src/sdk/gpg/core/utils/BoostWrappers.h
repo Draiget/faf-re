8faf19813b8dba22d57e8e30efac64cdf3cb8cf3
Vladislav Smetannikov <draiget@gmail.com>
2026-08-20 19:53:34 +0400

    Cite FUN_005CE460 on the existing SharedPtrRaw<T>::release() lane
    Third ICF-folded instantiation (boost::shared_ptr_CIntelGrid::release) of
    the already-recovered generic release() body -- confirmed genuinely
    invoked via ReleaseSharedCIntelGrid's pointer.release() call
    (ArchiveSerialization.cpp, ReadArchive::ReadPointerShared_CIntelGrid2),
    just missing from the fused Doxygen citation block.

--- src/sdk/gpg/core/utils/BoostWrappers.h
+++ src/sdk/gpg/core/utils/BoostWrappers.h
@@ -244,6 +244,7 @@ namespace boost
          * Address: 0x0053ACA0 (FUN_0053ACA0)
          * Address: 0x005BE250 (FUN_005BE250, Moho::WeakPtr_CIntelGrid::release)
          * Address: 0x0055FC60 (FUN_0055FC60, boost::shared_ptr_RScmResource::release)
+         * Address: 0x005CE460 (FUN_005CE460, boost::shared_ptr_CIntelGrid::release)
          *
          * What it does:
          * Releases one shared owner from the control block, disposes the pointee
